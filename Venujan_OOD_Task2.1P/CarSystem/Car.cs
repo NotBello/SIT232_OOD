ab0eec1e4aa32c8e9bf4cf794fa76d01eaf47a86
BELLO <BELLO@BELLO-PC>
2023-08-28 18:52:37 +0530

    The full car systems project with refuel and drive functions

---
+++ Venujan_OOD_Task2.1P/CarSystem/Car.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSystem
+{
+    class Car
+    {
+        //Instance Variables
+        private double fuelRate;
+        private double fuelLoad = 0;
+        private double totalMiles  = 0;
+
+        //Constants
+        private const double FUEL_COST = 3.82;  
+
+        //Constructors
+        public Car(double Rate, double Load, double Miles)
+        {
+            this.fuelRate = Rate;
+            this.fuelLoad = Load;
+            this.totalMiles = Miles;
+        }
+
+        //Accessors
+        public double getFuel()
+        {
+            return fuelLoad;
+        }
+
+        public double getFuelRate()
+        {
+            return fuelRate;
+        }
+
+        public double getTotalMiles()
+        {
+            return totalMiles;
+        }
+
+        //Mutators 
+        public void setTotalMiles(double TotalMiles)
+        {
+            this.totalMiles = TotalMiles;
+        }
+
+        //Private Methods
+        private double calcCost()
+        {
+            double cost = this.fuelLoad * FUEL_COST;
+            return cost; //Does not store the cost
+        }
+
+        private double convertToLitres(double gallons)
+        {
+            double liters = gallons * 4.546;
+            return liters;
+        }
+
+        //Public Methods
+        public string printFuelCost()
+        {
+            return this.calcCost().ToString("C");
+        }
+
+        public void addFuel(double fuelIntake)
+        {
+            this.fuelLoad += fuelIntake;
+            this.printFuelCost();
+        }
+        
+   
+        public void drive(double miles)
+        {
+            double gallonSpent;
+            double litreSpent;
+
+            Console.WriteLine("Before driving          " + miles + "miles");
+            Console.WriteLine("Fuel efficiency:        " + this.fuelRate);
+            Console.WriteLine("Fuel load (in litres):   " + this.fuelLoad);
+            Console.WriteLine("The total miles driven: " + this.totalMiles);
+
+            Console.WriteLine("\nAfter journey...");
+
+            this.totalMiles += miles;
+
+            Console.WriteLine();
+            gallonSpent = (miles / this.fuelRate);
+            Console.WriteLine("The gallons spent in " + miles + " miles: " +  gallonSpent);
+
+            litreSpent = convertToLitres(gallonSpent);
+            Console.WriteLine("The litres spent in " + miles + " miles: " + litreSpent);
+
+            this.fuelLoad -= litreSpent;
+            Console.WriteLine();
+            Console.WriteLine("After driving           " + miles + "miles");
+            Console.WriteLine("Fuel efficiency:        " + this.fuelRate);
+            Console.WriteLine("Fuel load (in litres):  " + this.fuelLoad);
+            Console.WriteLine("The total miles driven: " + this.totalMiles);
+            Console.WriteLine("Total cost:             " + printFuelCost());
+
+        }
+
+
+    }
+}
---
+++ Venujan_OOD_Task2.1P/CarSystem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CarSystem
+{
+    class CarProgram
+    {
+        static void Main(string[] args)
+        {
+            
+            Car bello = new Car(38.4, 50, 0);
+
+            double fuel = bello.getFuel();
+            double fuelInGallons = fuel / 4.546;
+            double mileLimit = fuelInGallons * bello.getFuelRate();
+
+            Console.WriteLine("You can travel upto " + mileLimit.ToString() + " miles, with the " + bello.getFuel() + " litres");
+
+            Console.WriteLine("\nbello starts journey...");
+            bello.drive(300);
+
+            Console.WriteLine("\nRefuelling for 20 lites");
+            bello.addFuel(20);
+            Console.WriteLine("\nYou can travel upto " + mileLimit.ToString() + " miles, with the " + bello.getFuel() + " litres");
+
+            Console.WriteLine("\nbello starts journey...");
+            bello.drive(200);
+
+        }
+    }
+}
