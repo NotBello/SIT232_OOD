7edfccd39201536a6403ac66e94f149417384b4f
BELLO <BELLO@BELLO-PC>
2023-08-28 15:56:34 +0530

    Starting Employee Finance management

---
+++ Venujan_OOD_Task2.1P/EmployeeSalaryManagment/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeSalaryManagment
+{
+    class Employee
+    {
+        //Instance Variables
+        private string name;
+        private double salary;
+
+        //Constructor
+        public Employee(string employeeName, double currentSalary)
+        {
+            this.name = employeeName;
+            this.salary = currentSalary;
+        }
+
+
+    }
+}
---
+++ Venujan_OOD_Task2.1P/EmployeeSalaryManagment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeSalaryManagement
+{
+    class EmployeeProgram
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
