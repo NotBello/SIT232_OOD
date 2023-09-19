cbf08ecba4a52e8a4b5114e369d6ce3ec0fef3e5
NotBello <venujan42@gmail.com>
2023-09-19 21:59:41 +0530

    Question 2

--- Venujan_OOD_Task3.2P/BankTestAndUI/BankTestAndUI/Program.cs
+++ Venujan_OOD_Task3.2P/BankTestAndUI/BankTestAndUI/Program.cs
@@ -2,13 +2,46 @@
 
 namespace bank
 {
-    
+
+    enum MenuOption
+    {
+        Withdraw,
+        Deposit,
+        Print,
+        Quit
+    }
 
     class BankSystem
     {
+
+        static MenuOption ReadUserOption()
+        {
+            MenuOption userChoice;
+            do
+            {
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1. Withdraw");
+                Console.WriteLine("2. Deposit");
+                Console.WriteLine("3. Print");
+                Console.WriteLine("4. Quit");
+
+
+                // Try to parse the userChoice into an enum value of MenuOption.
+                // Additionally check whether the value represented by userChoice is defined in the MenuOption enum.
+                if (Enum.TryParse(Console.ReadLine(), out userChoice) && Enum.IsDefined(typeof(MenuOption), userChoice))
+                {
+                    break;
+                }
+
+                Console.WriteLine("Invalid option. Please choose a valid option (0-3).");
+            } while (true);
+
+            return userChoice;
+        }
+
         static void Main(string[] args)
         {
-            
+            /*
             // 1st question
             Account bello = new Account(24000.99, "Venujan");
             bello.Print();
@@ -18,11 +51,44 @@ namespace bank
             Console.WriteLine("Deposit 10,000");
             Console.WriteLine("The transaction is " + bello.Deposit(10000));
             bello.Print();
-            
+            */
 
             // 2nd question
+            Account bello = new Account(24000.99, "Venujan");
+            bello.Print();
 
+            MenuOption userChoice;
+            do
+            {
+                userChoice = ReadUserOption();
 
+                switch (userChoice)
+                {
+                    case MenuOption.Withdraw:
+                        Console.Write("Enter the amount to withdraw: ");
+                        double withdrawAmount = Convert.ToDouble(Console.ReadLine());
+                        bool withdrawResult = bello.Withdraw(withdrawAmount);
+                        string withdrawResultString = (withdrawResult) ? "successful." : "unsuccessful."; //if else shorthand
+                        Console.WriteLine("The transaction is " + withdrawResultString);
+                        break;
+                    case MenuOption.Deposit:
+                        Console.Write("Enter the amount to deposit: ");
+                        double depositAmount = Convert.ToDouble(Console.ReadLine());
+                        bool depositResult = bello.Deposit(depositAmount);
+                        string depositResultString = (depositResult) ? "successful." : "unsuccessful.";//if else shorthand
+                        Console.WriteLine("The transaction is " + depositResultString);
+                        break;
+                    case MenuOption.Print:
+                        bello.Print();
+                        break;
+                    case MenuOption.Quit:
+                        Console.WriteLine("Goodbye!");
+                        break;
+                }
+            } while (userChoice != MenuOption.Quit);
         }
+
+
     }
 }
+
