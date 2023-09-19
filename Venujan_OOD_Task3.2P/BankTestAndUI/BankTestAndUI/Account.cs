4b27a5f017cf7d0c6497282334ba38fa8aa4b9f1
NotBello <venujan42@gmail.com>
2023-09-19 21:21:01 +0530

    separated classes into separate files

---
+++ Venujan_OOD_Task3.2P/BankTestAndUI/BankTestAndUI/Account.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank
+{
+    class Account
+    {
+        private double balance;
+        private string name;
+        private bool validateWithdraw = false;
+        private bool validateDeposit = false;
+
+
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+
+
+        public Account(double balance, string name)
+        {
+            this.balance = balance;
+            this.name = name;
+        }
+
+        public bool Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                validateDeposit = true;
+                this.balance += amount;
+            }
+            else
+            {
+                validateDeposit = false;
+            }
+            return validateDeposit;
+
+        }
+
+        public bool Withdraw(double amount)
+        {
+            if (this.balance > amount)
+            {
+                this.validateWithdraw = true;
+                this.balance -= amount;
+
+            }
+            else
+            {
+                validateWithdraw = false;
+            }
+            return validateWithdraw;
+
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Hey {this.name}, your balance is {this.balance}");
+        }
+
+
+    }
+}
---
+++ Venujan_OOD_Task3.2P/BankTestAndUI/BankTestAndUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace bank
+{
+    
+
+    class BankSystem
+    {
+        static void Main(string[] args)
+        {
+            
+            // 1st question
+            Account bello = new Account(24000.99, "Venujan");
+            bello.Print();
+            Console.WriteLine("Withdrawing 10,000");
+            Console.WriteLine("The transaction is " + bello.Withdraw(10000));
+            bello.Print();
+            Console.WriteLine("Deposit 10,000");
+            Console.WriteLine("The transaction is " + bello.Deposit(10000));
+            bello.Print();
+            
+
+            // 2nd question
+
+
+        }
+    }
+}
