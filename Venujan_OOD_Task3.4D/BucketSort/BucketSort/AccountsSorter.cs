5a8ca8d70964202575628c2fa55b708ef4f9c7a6
NotBello <venujan42@gmail.com>
2023-09-24 12:37:23 +0530

    Task 3.4D With Output ( Bucket Sort )

---
+++ Venujan_OOD_Task3.4D/BucketSort/BucketSort/AccountsSorter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace bank
+{
+    public static class AccountsSorter
+    {
+        public static void Sort(Account[] accounts, int b)
+        {
+            // Checking for empty set
+            if (accounts == null || accounts.Length == 0)
+            {
+                return;
+            }
+
+
+            List<Account>[] buckets = new List<Account>[b];
+            decimal M = GetMaximumBalance(accounts);
+
+            for (int i = 0; i < b; i++)
+            {
+                buckets[i] = new List<Account>();
+            }
+
+            foreach (Account acc in accounts)
+            {
+                int index = (int)Math.Ceiling(b * (double)acc.Balance / (double)M) - 1; // Use Math.Ceiling and adjust index calculation
+                buckets[index].Add(acc);
+            }
+
+            for (int i = 0; i < b; i++)
+            {
+                nextSort(buckets[i]);
+            }
+
+            int indexCount = 0;
+            for (int i = 0; i < b; i++)
+            {
+                foreach (Account acc in buckets[i])
+                {
+                    accounts[indexCount] = acc;
+                    indexCount++;
+                }
+            }
+        }
+
+
+        public static void Sort(List<Account> accounts, int b)
+        {
+            Account[] accountArray = accounts.ToArray();
+            Sort(accountArray, b);
+            accounts.Clear();
+            accounts.AddRange(accountArray);
+        }
+
+        private static void nextSort(List<Account> bucket)
+        {
+            bucket.Sort((a, b) => a.Balance.CompareTo(b.Balance));
+        }
+
+        /*
+         * 
+         */
+        private static decimal GetMaximumBalance(Account[] accounts)
+        {
+            decimal maxBalance = decimal.MinValue;
+            foreach (Account acc in accounts)
+            {
+                if (acc.Balance > maxBalance)
+                {
+                    maxBalance = acc.Balance;
+                }
+            }
+            return maxBalance;
+        }
+    }
+}
---
+++ Venujan_OOD_Task3.4D/BucketSort/BucketSort/Program.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * Written by Venujan Malaiyandi
+ * BSCP | CS | 61 | 101
+ * Bucket Sort for Accounts
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            // Test the AccountsSorter class for array
+            Account[] accountsArray = new Account[]
+            {
+                new Account(1000000, "Venujan"),
+                new Account(15000, "Saro"),
+                new Account(600, "Dev"),
+                new Account(20000, "David")
+            };
+
+            AccountsSorter.Sort(accountsArray, 4);
+
+            Console.WriteLine("Test the AccountsSorter class for arra");
+            Console.WriteLine("Sorted Accounts by Balance:");
+            foreach (Account acc in accountsArray)
+            {
+                acc.Print();
+            }
+
+            Console.WriteLine();
+
+            
+            // Test the AccountsSorter class for list
+            List<Account> accountsList = new List<Account>
+            {
+                new Account(1000000, "Venujan"),
+                new Account(15000, "Saro"),
+                new Account(6000, "Dev"),
+                new Account(20000, "David")
+            };
+
+            AccountsSorter.Sort(accountsList, 4);
+
+            Console.WriteLine("Test the AccountsSorter class for list");
+            Console.WriteLine("Sorted Accounts by Balance:");
+            foreach (Account acc in accountsList)
+            {
+                acc.Print();
+            }
+
+            // Test special cases
+            List<Account> emptyList = new List<Account>();
+            AccountsSorter.Sort(emptyList, 4); // Sorting an empty list should not cause any issues
+
+            Account[] nullArray = null;
+            AccountsSorter.Sort(nullArray, 4); // Sorting a null array should not cause any issues
+        }
+    }
+}
