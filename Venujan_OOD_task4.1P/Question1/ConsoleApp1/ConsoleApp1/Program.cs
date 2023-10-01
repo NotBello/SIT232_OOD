254fe3874000a88b4cc4c044390e2dd926024428
NotBello <venujan42@gmail.com>
2023-10-01 22:12:57 +0530

    Task4.1PPart1

---
+++ Venujan_OOD_task4.1P/Question1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace handleError
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            errorcases usrOne = new errorcases();
+
+            Console.WriteLine("The options:");
+            Console.WriteLine("Press 0 for NullReferenceException");
+            Console.WriteLine("Press 1 for IndexOutOfRangeException");
+            Console.WriteLine("Press 2 for StackOverflowException");
+            Console.WriteLine("Press 3 for OutOfMemoryException");
+            Console.WriteLine("Press 4 for InvalidCastException");
+            Console.WriteLine("Press 5 for DivideByZeroException");
+            Console.WriteLine("Press 6 for ArgumentException");
+            Console.WriteLine("Press 7 for ArgumentOutOfRangeException");
+            Console.WriteLine("Press 8 for SystemException");
+
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            switch (input)
+            {
+                case 0:
+                    usrOne.nullreferenceexception();
+                    break;
+                case 1:
+                    usrOne.indexoutofrangeexception();
+                    break;
+                case 2:
+                    usrOne.stackoverflowexception();
+                    break;
+                case 3:
+                    usrOne.outofmemoryexception();
+                    break;
+                case 4:
+                    usrOne.invalidcastexception();
+                    break;
+                case 5:
+                    usrOne.dividebyzeroexception();
+                    break;
+                case 6:
+                    usrOne.argumentexception();
+                    break;
+                case 7:
+                    usrOne.argumentoutofrangeexception();
+                    break;
+                case 8:
+                    usrOne.systemexception();
+                    break;
+            }
+        }
+    }
+}
---
+++ Venujan_OOD_task4.1P/Question1/ConsoleApp1/ConsoleApp1/errorcases.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace handleError
+{
+    internal class errorcases
+    {
+        public void nullreferenceexception()
+        {
+            string str = null;
+            int length = str.Length; // This line will throw a NullReferenceException
+
+        }
+
+        public void indexoutofrangeexception()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            int sixthElement = numbers[5]; // This line will throw an IndexOutOfRangeException
+        }
+
+        private static int RecursiveFunction(int n)
+        {
+            return RecursiveFunction(n + 1); // Recursive call without termination condition
+        }
+
+        public void stackoverflowexception()
+        {
+            
+            // Call the recursive function
+            int result = RecursiveFunction(1);
+
+        }
+
+        public void outofmemoryexception()
+        {
+            List<byte[]> listOfArrays = new List<byte[]>();
+
+            while (true)
+            {
+                byte[] newArray = new byte[1000000]; // Allocating a large array
+                listOfArrays.Add(newArray);
+            }
+
+        }
+
+        public void invalidcastexception()
+        {
+            object myObject = "Hello";
+            int myInt = (int)myObject; // This line will throw an InvalidCastException
+
+        }
+
+        public void dividebyzeroexception()
+        {
+            //int result = 10 / 0; // This line will throw a DivideByZeroException
+
+        }
+
+        public void Divide(int dividend, int divisor)
+        {
+            if (divisor == 0)
+            {
+                throw new ArgumentException("Divisor cannot be zero.", nameof(divisor));
+            }
+            int result = dividend / divisor;
+        }
+
+        public void argumentexception()
+        {
+            Divide(1,0);
+        }
+
+        public void argumentoutofrangeexception()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            int index = 10;
+            int value = numbers[index]; // This line will throw an ArgumentOutOfRangeException
+        }
+
+        static void SimulateSystemError()
+        {
+            // Simulating a system-related error
+            throw new SystemException("This is a simulated SystemException.");
+        }
+
+        public void systemexception()
+        {
+            try
+            {
+                SimulateSystemError();
+            }
+            catch (SystemException ex)
+            {
+                Console.WriteLine("Caught a SystemException:");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+    }
+}
